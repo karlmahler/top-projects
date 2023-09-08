54e52abc693b02591698e305bd832f523c0bebe1
Hasley <raudineac101@gmail.com>
2023-09-08 18:24:01 -0400

    Add unit tests

---
+++ ruby/caesar_cipher/spec/caesar_cipher_spec.rb
@@ -0,0 +1,25 @@
+require './lib/caesar_cipher'
+
+describe CaesarCipher do
+  describe '#cipher' do
+    it 'encrypts text with a shift factor of 13' do
+      expect(CaesarCipher.cipher('hello world', 13)).to eql('uryyb jbeyq')
+    end
+
+    it 'encrypts text with a shift factor of 10' do
+      expect(CaesarCipher.cipher('hello world', 10)).to eql('rovvy gybvn')
+    end
+
+    it 'encrypts text with a negative shift factor' do
+      expect(CaesarCipher.cipher('hello world', -16)).to eql('rovvy gybvn')
+    end
+
+    it 'respects capitalization' do
+      expect(CaesarCipher.cipher('Hello WORLD', 13)).to eql('Uryyb JBEYQ')
+    end
+
+    it 'respect punctuation' do
+      expect(CaesarCipher.cipher('hello, world!', 13)).to eql('uryyb, jbeyq!')
+    end
+  end
+end
